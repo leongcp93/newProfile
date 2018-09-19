04aeeb80d8be6de95c3958887ef28e961f520759
Patrick <patrickleongcp@gmail.com>
2018-09-19 17:40:39 +1000

    updated: added footer & fixed: form alignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducationSectionComponent } from './education-section/education-section.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { WorkingExperiencesComponent } from './working-experiences/working-experiences.component';
+import { FooterSectionComponent } from './footer-section/footer-section.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectComponent } from './project/project.component';
+import { ViewResumeComponent } from './view-resume/view-resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationSectionComponent,
+    ContactFormComponent,
+    WorkingExperiencesComponent,
+    FooterSectionComponent,
+    MainPageComponent,
+    IntroductionComponent,
+    AboutMeComponent,
+    ProjectComponent,
+    ViewResumeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/working-experiences/working-experiences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-working-experiences',
+  templateUrl: './working-experiences.component.html',
+  styleUrls: ['./working-experiences.component.scss']
+})
+export class WorkingExperiencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
